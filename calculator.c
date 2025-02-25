9c173255b975b8500e104da1ec8ad8c44f17b256
manas-manna <madanmanna123456@gmail.com>
2025-02-26 03:25:55 +0530

    fix

--- calculator.c
+++ calculator.c
@@ -14,7 +14,7 @@ long long factorial(int n) {
 int main() {
     int choice;
     double x, y;
-    
+
     while (1) {
         printf("\nScientific Calculator\n");
         printf("1. Square Root (√x)\n");
@@ -22,42 +22,40 @@ int main() {
         printf("3. Natural Logarithm (ln(x))\n");
         printf("4. Power (xʸ)\n");
         printf("5. Exit\n");
-        printf("Please enter your choice: ");
-        scanf("%d", &choice);
+        printf("Enter your choice: "); 
+        fflush(stdout); 
 
+        scanf("%d", &choice);
+        
         switch (choice) {
             case 1:
                 printf("Enter number: ");
+                fflush(stdout);
                 scanf("%lf", &x);
-                if (x < 0)
-                    printf("Invalid input! Square root of negative number is not defined.\n");
-                else
-                    printf("Result: %.2lf\n", sqrt(x));
+                printf("Result: %.2lf\n", sqrt(x));
                 break;
             
             case 2:
                 printf("Enter a non-negative integer: ");
+                fflush(stdout);
                 int num;
                 scanf("%d", &num);
-                if (num < 0)
-                    printf("Invalid input! Factorial of negative number is not defined.\n");
-                else
-                    printf("Result: %lld\n", factorial(num));
+                printf("Result: %lld\n", factorial(num));
                 break;
             
             case 3:
                 printf("Enter number: ");
+                fflush(stdout);
                 scanf("%lf", &x);
-                if (x <= 0)
-                    printf("Invalid input! Logarithm is not defined for zero or negative numbers.\n");
-                else
-                    printf("Result: %.2lf\n", log(x));
+                printf("Result: %.2lf\n", log(x));
                 break;
             
             case 4:
                 printf("Enter base (x): ");
+                fflush(stdout);
                 scanf("%lf", &x);
                 printf("Enter exponent (y): ");
+                fflush(stdout);
                 scanf("%lf", &y);
                 printf("Result: %.2lf\n", pow(x, y));
                 break;
@@ -72,3 +70,4 @@ int main() {
     }
     return 0;
 }
+
